07112ec10812738e23d1d5bbc65258f5f5dda792
Dean Rotondella <128656966+Dean2500@users.noreply.github.com>
2024-06-06 22:20:59 +0800

    code overhall
    The portfolio has been  improved, with significant CSS updates and HTML adjustments.

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
- function changeContent(id) {
-     var contents = document.querySelectorAll('#main > div');
-     for (var i = 0; i < contents.length; i++) {
-         contents[i].style.display = 'none';
-     }
-     document.getElementById(id).style.display = 'block';
+/**
+ * Swap the displayed content to the div
+ * that matches the provided id
+ * @param {*} id 
+ */
+function changeContent(id) {
+     document.querySelectorAll('#main > div').forEach(function(element) {
+        //check to see if the current element id matches the id 
+        //that was requested to be displayed and makes it visible if it does
+        if( id == element.id )
+        {
+            element.style.display = 'block';
+        }
+
+        //hide the field
+        else
+        {
+            element.style.display = 'none';
+        }
+       
+    });
+    
  }
+
+ 
 
   
